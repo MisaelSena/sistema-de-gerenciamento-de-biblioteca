0add7f6427b3588c3808c5fcbe3dd4da50a6a955
Misael Sena <misaellsena@gmail.com>
2023-08-14 20:43:33 -0300

    Objetos Aluno e Professor instanciados

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 //Projeto Biblioteca
+import { Usuario } from "./Usuario";
+import { Aluno } from "./Aluno";
+import { Professor } from "./Professor";
+
+const aluno1 = new Aluno(1, "Elthon", 123, "Backend");
+const professor1 = new Professor(2,"Alcides", aluno1);
+
+console.log(aluno1);
+console.log(professor1);
